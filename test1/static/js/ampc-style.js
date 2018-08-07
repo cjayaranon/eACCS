4f5c123210e3e1d942e92213891d14d695a961e4
Cyril John <cjayaranon@gmail.com>
2018-08-07 18:00:00 +0800

    input filters
    [1] height filter added using input masking
    [2] monthly income filter added using input masking

--- test1/static/js/ampc-style.js
+++ test1/static/js/ampc-style.js
@@ -1,12 +1,60 @@
-var text = document.getElementById('monthly-income');
-text.onkeypress = text.onpaste = checkInput;
+(function($, undefined) {
 
-function checkInput(e) {
-    var e = e || event;
-    var char = e.type == 'keypress' 
-        ? String.fromCharCode(e.keyCode || e.which) 
-        : (e.clipboardData || window.clipboardData).getData('Text');
-    if (/[^(?!0\.00)[1-9]\d{0,2}(,\d{3})*(\.\d\d)?$]/gi.test(char)) {
-        return false;
-    }
-}
+    "use strict";
+
+    // When ready.
+    $(function() {
+        
+        var $form = $( "#form" );
+//        var $input = $form.find( "input" );
+        var $input = $('#monthly-income');
+
+        $input.on( "keyup", function( event ) {
+            
+            
+            // When user select text in the document, also abort.
+            var selection = window.getSelection().toString();
+            if ( selection !== '' ) {
+                return;
+            }
+            
+            // When the arrow keys are pressed, abort.
+            if ( $.inArray( event.keyCode, [38,40,37,39] ) !== -1 ) {
+                return;
+            }
+            
+            
+            var $this = $( this );
+            
+            // Get the value.
+            var input = $this.val();
+            
+            var input = input.replace(/[\D\s\._\-]+/g, "");
+                    input = input ? parseInt( input, 10 ) : 0;
+
+                    $this.val( function() {
+                        return ( input === 0 ) ? "" : input.toLocaleString( "en-US" );
+                    } );
+        } );
+        
+        /**
+         * ==================================
+         * When Form Submitted
+         * ==================================
+         */
+        $form.on( "submit", function( event ) {
+            
+            var $this = $( this );
+            var arr = $this.serializeArray();
+        
+            for (var i = 0; i < arr.length; i++) {
+                    arr[i].value = arr[i].value.replace(/[($)\s\._\-]+/g, ''); // Sanitize the values.
+            };
+            
+            console.log( arr );
+            
+            event.preventDefault();
+        });
+        
+    });
+})(jQuery);
